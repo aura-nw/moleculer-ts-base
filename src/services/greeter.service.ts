3b3520f246630a69d145ca6ba32b7bc907e1e474
Tuan Bui <tuanba1@gmail.com>
2022-12-09 16:26:33 +0700

    reorgnize: move source code to src folder

---
+++ src/services/greeter.service.ts
@@ -0,0 +1,40 @@
+import { Service, ServiceBroker, Context } from 'moleculer';
+
+export default class GreeterService extends Service {
+    public constructor(public broker: ServiceBroker) {
+        super(broker);
+        this.parseServiceSchema({
+            name: 'greeter',
+            actions: {
+                hello: {
+                    rest: {
+                        method: 'GET',
+                        path: '/hello',
+                    },
+                    async handler(): Promise<string> {
+                        return this.ActionHello();
+                    },
+                },
+
+                welcome: {
+                    rest: '/welcome',
+                    params: {
+                        name: 'string',
+                    },
+                    async handler(ctx: Context<{ name: string }>): Promise<string> {
+                        return this.ActionWelcome(ctx.params.name);
+                    },
+                },
+            },
+        });
+    }
+
+    // Action
+    public ActionHello(): string {
+        return 'Hello Moleculer';
+    }
+
+    public ActionWelcome(name: string): string {
+        return `Welcome, ${name}`;
+    }
+}
