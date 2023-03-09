4ccde892931bc2af5f34dcf1f55b65944e7c1181
Tuan Bui <tuanba1@gmail.com>
2023-03-09 14:04:40 +0700

    fix: add migration script

--- migrations/20230110082623_create-test.ts
+++ migrations/20230110082623_create-test.ts
@@ -1,5 +1,25 @@
 import { Knex } from 'knex';
 
-export async function up(knex: Knex): Promise<void> {}
+export async function up(knex: Knex): Promise<void> {
+  let tblName = 'persons';
+  await knex.schema.dropTableIfExists(tblName);
+  await knex.schema.createTable(tblName, (table: any) => {
+    table.increments('id').primary();
+    table.string('firstName');
+    table.string('lastName');
+    // table.string('name').unique().notNullable();
+    // table.string('lp_contract_addr').unique().notNullable();
+    // table.string('contract_addr').unique().notNullable();
+    // table.string('pool_contract_addr').unique().notNullable();
+    // table.integer('fee').notNullable();
+    // table.string('whitelist_addr').defaultTo('');
+    // table.string('creator_addr').defaultTo('');
+    // table.integer('lp_total').notNullable();
+    // table.string('status').notNullable();
+    table.timestamp('created_at').notNullable().defaultTo(knex.raw('now()'));
+    table.timestamp('updated_at').notNullable().defaultTo(knex.raw('now()'));
+    table.timestamp('delete_at').nullable();
+  });
+}
 
 export async function down(knex: Knex): Promise<void> {}
