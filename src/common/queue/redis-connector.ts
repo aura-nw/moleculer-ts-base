fad3f58dd691dc1559852f30a9e4ab869e626d6c
Tuan Bui <tuanba1@gmail.com>
2023-03-22 17:07:28 +0700

    fix: remove direct reference to IORedis

--- src/common/queue/redis-connector.ts
+++ src/common/queue/redis-connector.ts
@@ -1,20 +1,29 @@
 // TODO: add logic to inject redis information here instead of get from environment
-import IORedis, { RedisOptions } from 'ioredis';
+
+import { RedisOptions } from 'bullmq';
 
 // let path = process.env.BULL_REDIS_URL
 // const redisConnection = getRedisConnection(path);
 // export default redisConnection;
 
-export function getRedisConnection(path?: string): IORedis {
+export function getRedisConnection(path?: string): RedisOptions {
   return getIORedisInstance(path);
 }
 
-let _ioRedis: IORedis;
-function getIORedisInstance(path?: string): IORedis {
-  if (_ioRedis) return _ioRedis;
+function getIORedisInstance(path?: string): RedisOptions {
+  const _path = path ?? 'redis://localhost:6379';
+  const url = new URL(_path);
 
-  // no redisconnection, create one
-  const opt: RedisOptions = { maxRetriesPerRequest: null };
-  _ioRedis = path ? new IORedis(path, opt) : new IORedis(opt);
-  return _ioRedis;
+  const res = {
+    host: url.hostname || 'localhost',
+    port: parseInt(url.port, 10) || 6379,
+  };
+
+  return res;
+  // if (_ioRedis) return _ioRedis;
+  //
+  // // no redisconnection, create one
+  // const opt: RedisOptions = { maxRetriesPerRequest: null , };
+  // _ioRedis = path ? new IORedis(path, opt) : new IORedis(opt);
+  // return _ioRedis;
 }
